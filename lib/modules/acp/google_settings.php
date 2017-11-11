2af50f7a50983d3f2f090b5041c05f5f0571819b
rain1 <rain@oksvort.eu>
2017-11-11 19:42:18 +0200

    Create settings for google fragment

---
+++ lib/modules/acp/google_settings.php
@@ -0,0 +1,54 @@
+<?php
+class google_settings{
+    var $module_info = array(
+        'title' => "logs",
+        'MODULES' => array(
+            array('name' => 'google_settings','permissions' => 'a_manage_board_settings'),
+        )
+    );
+    
+
+    function main($module){
+        global $language;
+
+    $lng = array(
+        'general_labels' => $language['setting_name'],
+        'general_descriptions' => $language['setting_description'],
+    );
+    
+    $lng_js = array_to_js($lng,"lang",true, true);
+
+        switch($module){
+            case 'google_settings':
+                if(isset($_GET['mode'])){
+                    if($_GET['mode'] == "boardsettingssave"){
+                        if(to_base_general("google")){
+                            $this->template = "success_module";
+                            $this->vars=array(
+                                'SUCCESSMSG' => $language['notifications']['settings_board_update']."<br><br><a href='./acp.php?id=".$_GET['id']."&a=".$_GET['a']."' style='color: #EEEEEE;'><b>Click here to go back</b></a>"
+                            );
+                        }else{
+                            $this->template = "failure_module";
+                            $this->vars=array(
+                                'SUCCESSMSG' => $language['notifications']['settings_board_update']."<br><br><a href='./acp.php?id=".$_GET['id']."&a=".$_GET['a']."' style='color: #EEEEEE;'><b>Click here to go back</b></a>"
+                            );
+                        }
+                    }
+                    break;
+                }
+                $this->page_title = $language['module_titles']['google_settings'];
+                $this->template = "board_settings";
+                $permissions = get_table_contents("general", 'ALL'," WHERE readonly=0 AND class='google'");
+                $js = array_to_js($permissions,"general",true, true);
+                $this->vars=array(
+                    'settings' => $js,
+                    'LANGUAGE' => $lng_js,
+                    'TEMPLATES' => '',
+                    'LANGUAGE_COMBO' => ''
+                );
+            break;
+        }
+    }
+}
+
+?>
